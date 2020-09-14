2d6f4d61db085ee11b190e60a7f6389347319568
Hearty, Oh <heartyoh@hatiolab.com>
2020-09-14 12:03:31 +0900

    fix mdns issue on linux
    https://stackoverflow.com/questions/29589543/raspberry-pi-mdns-getaddrinfo-3008-error
    https://github.com/agnat/node_mdns/issues/130

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -1,15 +1,21 @@
 var mdns = require('mdns')
 var browser
 var services = {}
-process.on('bootstrap-module-middleware' as any, async app => {
+process.on('bootstrap-module-middleware' as any, async (app) => {
   /* app에 middleware를 추가할 수 있다. */
-  browser = mdns.createBrowser(mdns.tcp('thingsfactory'))
+  var sequence = [
+    mdns.rst.DNSServiceResolve(),
+    'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({ families: [4] }),
+    mdns.rst.makeAddressesUnique(),
+  ]
 
-  browser.on('serviceUp', service => {
+  browser = mdns.createBrowser(mdns.tcp('thingsfactory'), { resolverSequence: sequence })
+
+  browser.on('serviceUp', (service) => {
     services[service.name] = service
     ;(global as any).screencastServices = [...Object.values(services)]
   })
-  browser.on('serviceDown', service => {
+  browser.on('serviceDown', (service) => {
     delete services[service.name]
     ;(global as any).screencastServices = [...Object.values(services)]
   })
@@ -17,7 +23,7 @@ process.on('bootstrap-module-middleware' as any, async app => {
   browser.start()
 })
 
-process.on('exit' as any, code => {
+process.on('exit' as any, (code) => {
   if (browser) {
     browser.stop()
     browser = null
