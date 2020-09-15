0b378fe4ec63115f1c2549c52cc8d1f17dfa9138
Hearty, Oh <heartyoh@hatiolab.com>
2020-09-16 08:20:06 +0900

    try..catch for mdns fault

--- server/middlewares/index.ts
+++ server/middlewares/index.ts
@@ -1,26 +1,32 @@
 var mdns = require('mdns')
 var browser
 var services = {}
+
 process.on('bootstrap-module-middleware' as any, async (app) => {
-  /* app에 middleware를 추가할 수 있다. */
-  var sequence = [
-    mdns.rst.DNSServiceResolve(),
-    'DNSServiceGetAddrInfo' in mdns.dns_sd ? mdns.rst.DNSServiceGetAddrInfo() : mdns.rst.getaddrinfo({ families: [4] }),
-    mdns.rst.makeAddressesUnique(),
-  ]
+  try {
+    var sequence = [
+      mdns.rst.DNSServiceResolve(),
+      'DNSServiceGetAddrInfo' in mdns.dns_sd
+        ? mdns.rst.DNSServiceGetAddrInfo()
+        : mdns.rst.getaddrinfo({ families: [4] }),
+      mdns.rst.makeAddressesUnique(),
+    ]
 
-  browser = mdns.createBrowser(mdns.tcp('thingsfactory'), { resolverSequence: sequence })
+    browser = mdns.createBrowser(mdns.tcp('thingsfactory'), { resolverSequence: sequence })
 
-  browser.on('serviceUp', (service) => {
-    services[service.name] = service
-    ;(global as any).screencastServices = [...Object.values(services)]
-  })
-  browser.on('serviceDown', (service) => {
-    delete services[service.name]
-    ;(global as any).screencastServices = [...Object.values(services)]
-  })
+    browser.on('serviceUp', (service) => {
+      services[service.name] = service
+      ;(global as any).screencastServices = [...Object.values(services)]
+    })
+    browser.on('serviceDown', (service) => {
+      delete services[service.name]
+      ;(global as any).screencastServices = [...Object.values(services)]
+    })
 
-  browser.start()
+    browser.start()
+  } catch (e) {
+    console.error(e)
+  }
 })
 
 process.on('exit' as any, (code) => {
